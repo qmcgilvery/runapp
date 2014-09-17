5d8ffffb0776b5d55a1efdafcc0758871ae226ab
Quentin McGilvery <qmcgilvery12@gmail.com>
2014-09-17 00:22:43 -0700

    first commit

---
+++ accelerometer.js
@@ -0,0 +1,29 @@
+accelerometer
+
+						<!--<script>
+						// Wait for device API libraries to load
+						//
+						document.addEventListener("deviceready", onDeviceReady, false);
+
+						// device APIs are available
+						//
+						function onDeviceReady() {
+							navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+						}
+
+						// onSuccess: Get a snapshot of the current acceleration
+						//
+						function onSuccess(acceleration) {
+							alert('Acceleration X: ' + acceleration.x + '\n' +
+								  'Acceleration Y: ' + acceleration.y + '\n' +
+								  'Acceleration Z: ' + acceleration.z + '\n' +
+								  'Timestamp: '      + acceleration.timestamp + '\n');
+						}
+
+						// onError: Failed to get the acceleration
+						//
+						function onError() {
+							alert('onError!');
+						}
+
+						</script>-->
---
+++ header.php
@@ -0,0 +1,61 @@
+<div class="container-fluid">
+
+<div id="header" class="row-fluid">
+	  <div id="nav" class="span1">
+	  <a href="index.php">Home</a>
+	  </div>
+	  <div id="nav" class="span1">
+	  <a href="about.html">About</a>
+	  </div>
+	  <div id="nav" class="span1">
+	  <a href="schedule.html">Gallery</a>
+	  </div>
+	  <div id="nav" class="span1">
+	  <a href="contact.html">Contact</a>
+	  </div>
+	  <div class="span4">
+	  </div>
+	  <div class="span1">
+	  <a href="http://instagram.com/samofit#"><img src="../images/1380332669_38-instagram.png"/></a>
+	  </div>
+	  <div class="span1">
+      <a href="https://twitter.com/SaMoFit"><img src="../images/1380331883_03-twitter.png"/></a>
+	  </div>
+	  <div class="span1">
+      <a href="https://www.facebook.com/pages/SaMo-Fit/192082164191323?ref=hl"><img src="../images/1380331825_01.png"/></a>
+	  </div>
+	  <div class="span1">
+      <a href="https://vimeo.com/user13227037"><img src="../images/1380332027_13-vimeo.png"/></a>
+	  </div>
+</div>	  
+
+
+            <!--<div id="menu">
+                
+            	<ul>
+                	<li class="menu">
+                    	<a href="index.php">Home</a>
+                    </li><li class="menu">
+                    	<a href="about.html">About Us</a>
+                    </li><li class="menu">
+                    	<a href="schedule.html">Gallery</a>
+                    </li><li class="menu">
+                    	<a href="contact.html">Contact Us</a>
+                        
+                    </li><li style="border:none" class="socialIcon">                  
+                   		<a href="http://instagram.com/samofit#"><img src="../images/1380332669_38-instagram.png"/></a>
+                    </li><li class="socialIcon">
+                    	<a href="https://twitter.com/SaMoFit"><img src="../images/1380331883_03-twitter.png"/></a>
+                    </li><li class="socialIcon">
+                    	<a href="https://www.facebook.com/pages/SaMo-Fit/192082164191323?ref=hl"><img src="../images/1380331825_01.png"/></a>
+                    </li><li class="socialIcon">
+                    	<a href="https://vimeo.com/user13227037"><img src="../images/1380332027_13-vimeo.png"/></a>
+                    </li>
+                </ul>
+  </div>-->               
+            <div class="clear"></div>
+            
+			<div class="hero-unit">
+            <img id="logo" src="../../images/logo.png">
+			
+		
